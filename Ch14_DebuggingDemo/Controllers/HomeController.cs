a624bcf1562fc6c68da3d5270f759bc47c605ffe
Duy Pham <wosea@outlook.com>
2020-08-25 14:29:16 +0700

    working with ch14

---
+++ Ch14_DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch14_DebuggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 2;
+            int result = firstVal / secondVal;
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            return View(result+2);
+        }
+
+      
+    }
+}
---
+++ Ch14_DebuggingDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ch14_DebuggingDemo.Startup))]
+namespace Ch14_DebuggingDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
